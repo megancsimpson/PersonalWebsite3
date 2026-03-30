4435a23718260550367032191cd67fa85e467008
megancsimpson <megan@simpsonclan.org>
2026-03-30 13:31:16 -0700

    Fixing input and output mistakes

--- PersonalWebsite.js
+++ PersonalWebsite.js
@@ -2,27 +2,29 @@
 
 const button = document.getElementById("fetch-btn");
 const output = document.getElementById("output");
+const input = document.getElementById("input");
 
 button.addEventListener("click", clickHandler);
 
 async function clickHandler(){
+    const query = input.value;
     // fetch call
-    const response = await fetch(); //insert API call here
+    const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + encodeURIComponent(query) + "&appid=257e2f3c6f8b16ad584adc85cf805e1d&units=metric"); //insert API call here
     // could also do .then instead of await, but its longer
     // parse json data
     const data = await response.json();
     console.log(data);
 
-    const temp = document.createElement('p');  
+    const temp = document.createElement('h6');  
     // <p> title: ____</p> / tell it what to print / p creates a paragraph, could be h1 or even div
-    temp.textContent = "" + data.temp;
+    temp.textContent = "Temperature: " + data.main.temp;
 
     // document refers to DOM tree (website), data refers to JSON data
-    const feelsLike = document.createElement("p");
-    feelsLike.textContent = "" + data.feels_like;
+    const feelsLike = document.createElement("h6");
+    feelsLike.textContent = "Feels like: " + data.main.feels_like;
 
-    const description = document.createElement("p");
-    description.textContent = "" + data.description;
+    const description = document.createElement("h6");
+    description.textContent = "Description: " + data.weather[0].description;
 
     // print out for user
     output.appendChild(temp);
