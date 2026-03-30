0a896fc666145f72bb97b519aa62689d1288a8f6
megancsimpson <megan@simpsonclan.org>
2026-03-30 14:04:51 -0700

    Adding city name

--- PersonalWebsite.js
+++ PersonalWebsite.js
@@ -7,26 +7,31 @@ const input = document.getElementById("input");
 button.addEventListener("click", clickHandler);
 
 async function clickHandler(){
+    // Clear old results
+    output.innerHTML = "";
+    // Making the input value
     const query = input.value;
     // fetch call
-    const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + encodeURIComponent(query) + "&appid=257e2f3c6f8b16ad584adc85cf805e1d&units=metric"); //insert API call here
-    // could also do .then instead of await, but its longer
+    const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + encodeURIComponent(query) + "&appid=257e2f3c6f8b16ad584adc85cf805e1d&units=metric");
     // parse json data
     const data = await response.json();
-    console.log(data);
 
-    const temp = document.createElement('h6');  
+    const name = document.createElement("h5");
+    name.textContent = data.name + " Weather"
+
+    const temp = document.createElement("h6");  
     // <p> title: ____</p> / tell it what to print / p creates a paragraph, could be h1 or even div
-    temp.textContent = "Temperature: " + data.main.temp;
+    temp.textContent = "Temperature: " + data.main.temp + " degrees";
 
     // document refers to DOM tree (website), data refers to JSON data
     const feelsLike = document.createElement("h6");
-    feelsLike.textContent = "Feels like: " + data.main.feels_like;
+    feelsLike.textContent = "Feels like: " + data.main.feels_like + " degrees";
 
     const description = document.createElement("h6");
     description.textContent = "Description: " + data.weather[0].description;
 
     // print out for user
+    output.appendChild(name);
     output.appendChild(temp);
     output.appendChild(feelsLike);
     output.appendChild(description);
